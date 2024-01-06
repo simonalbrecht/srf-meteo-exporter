a7e19c6ff58987c49cc97c5aafa12ef098b11219
Simon Albrecht <simon@simonalbrecht.ch>
2024-01-06 15:40:48 +0100

    feat: delete obsolete scripts

--- scripts/publish-docker-image.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-set -e
-
-IMAGE="srf-meteo-exporter"
-VERSION=$(cat package.json | jq -r .version)
-
-docker push "ghcr.io/simonalbrecht/${IMAGE}:latest"
-docker push "ghcr.io/simonalbrecht/${IMAGE}:${VERSION}"
