9fb6870aed2c43741c4eefbe131dc68fef7b9b5b
Simon Albrecht <simon@simonalbrecht.ch>
2024-01-06 14:41:27 +0100

    feat: add root page with links

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -3,6 +3,21 @@ import { FastifyReply, FastifyRequest } from 'fastify'
 export const handle = (_: FastifyRequest, reply: FastifyReply) => {
     return reply
         .code(200)
-        .header('Content-Type', 'text/plain; charset=utf-8')
-        .send('SRF Meteo Exporter')
+        .header('Content-Type', 'text/html; charset=utf-8')
+        .send(
+            `<html>
+            <head>
+                <title>SRF Meteo Exporter</title>
+            </head>
+            <body>
+                <h1>SRF Meteo Exporter</h1>
+                <ul>
+                    <li><a href="/metrics">Metrics</a>
+                    <li><a href="/debug/state">Debug: State</a>
+                    <li><a href="/debug/cache">Debug: Cache</a>
+                </ul>
+            </body>
+            </html>
+        `
+        )
 }
